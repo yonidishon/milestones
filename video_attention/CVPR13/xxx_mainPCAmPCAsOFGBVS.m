6c83ae4c54ccd79b6c8d408a4bb78ce0e553d8d8
Yonatan Dishon <Yonatan Dishon>
2016-12-24 18:07:21 +0200

    Train+Test of cand+features using GBVS+OF+PCAm+PCAs

---
+++ video_attention/CVPR13/xxx_mainPCAmPCAsOFGBVS.m
@@ -0,0 +1,2 @@
+run C:\Users\ydishon\Documents\milestones\video_attention\CVPR13\xxx_cvpr13_jumpTrainPCAmPCAsOFGBVS.m
+run C:\Users\ydishon\Documents\milestones\video_attention\CVPR13\xxx_run_jumpTestPCAmPCAsOFGBVS.m
