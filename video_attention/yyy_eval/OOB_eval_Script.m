29885fc8f64bd31c086de88478c4f011d2291b79
yonidishon <yoni_dishon@yahoo.com>
2016-12-25 00:42:34 +0200

    Script to find all saved models and evaluate top-ones (hard coded)

--- video_attention/yyy_eval/OOB_eval_Script.m
+++ video_attention/yyy_eval/OOB_eval_Script.m
@@ -1,18 +1,52 @@
 % This script checks the OOB error of different models learned by me or by Dimtry
 
-% First get all the models learned that I have on the computer:
-addpath(genpath('C:\Users\yoni\Documents\milestones\video_attention\yyy_toolbox'));
+% 1. get all the models learned that I have on the computer:
+addpath(genpath('C:\Users\ydishon\Documents\milestones\video_attention\yyy_toolbox'));
 list_orig=subdir('\\cgm47\d\Dimtry_orig\DIEM');
 list_my=subdir('\\cgm47\d\Dima_Analysis_Milestones');
 list_submit=subdir('\\cgm47\D\DimaReleaseCode_CGMwebsite');
 tot_list = [list_orig';list_my';list_submit'];
 
-models =[];
+%% 2. Getting all trained models
+models ={};
 for ii=1:length(tot_list)
     found = dir(sprintf('%s/00_trained_model*.mat',tot_list{ii}));
     if ~isempty(found)
-        models = [models;fullfile(tot_list{ii},found.name)];
+        for jj=1:length(found)
+            models = [models;fullfile(tot_list{ii},found(jj).name)];
+        end
     end
 end
+
+%% 3. Plotting the OOB error rate vs iteration;
 figure('Name','OOB error rate');
-    plot(model.errtr(:,1)); title('OOB error rate');  xlabel('iteration (# trees)'); ylabel('OOB error rate');
+chosen_models = [models(6);models([28,31,33,34])];
+flags = ones(length(chosen_models),1);
+for ii=1:length(chosen_models)
+    model = load(chosen_models{ii});
+    if isfield(model.rf,'errtr')
+        plot(model.rf.errtr(:,1));
+    else
+        fprintf('Model:: %s has no OOB error\n',chosen_models{ii});
+        flags(ii)=0;
+        continue;
+    end
+    hold on;
+    drawnow;
+    grid on;
+end
+hold off;
+title('OOB error rate');
+xlabel('iteration (# trees)');
+ylabel('OOB error rate');
+legend(chosen_models(boolean(flags)),'Interpreter', 'none');
+%% 4. Getting the importantcy of each feature
+for ii=1:length(chosen_models)
+    figure('Name',chosen_models{ii})
+    model = load(chosen_models{ii});
+    bar(model.rf.importance(:,1));xlabel('feature');ylabel('magnitude');
+    title('Feature Improtance');   
+    drawnow;
+    grid on;
+end
+
