2b1c3ea4557f356e7c87ecbf6e0084bd10765113
Yonatan Dishon <ydishon@ef.technion.ac.il>
2017-09-16 00:09:20 +0300

    Only Partial features experiment (my candidates)

---
+++ video_attention/CVPR13/xxx_mainPCAmPCAsOFGBVS_onlyOFGBVS_Feat_20170915.m
@@ -0,0 +1,13 @@
+% Date: 15/09/2017
+% Experiment goal:
+%   Prove that my saliency maps contribute to classification accuracy
+%   How: 
+%       1. Use both mine and OF+GBVS candidates but do not use my features in
+%       The feature vector.
+%       2. Use both mine and OF+GBVS candidates but do not use S-PCA in my features in
+%       The feature vector.
+%       3. Compare the results of (1) and (2) to the original
+%       PCAmPCAsOFGBVS performance.
+% (1) step:
+run C:\Users\ydishon\Documents\milestones\video_attention\CVPR13\xxx_cvpr13_jumpTrainPCAmPCAsOFGBVS_FeatOFGBVS_20170915.m
+run C:\Users\ydishon\Documents\milestones\video_attention\CVPR13\xxx_run_jumpTestPCAmPCAsOFGBVS_FeatOFGBVS_20170915.m
